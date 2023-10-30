cdad815ec47f64bcf5a0fc9bf60c79d85089aac3
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-30 21:47:17 +0530

    Update p10.js

--- JS PROBLEM/p10.js
+++ JS PROBLEM/p10.js
@@ -1,1 +1,17 @@
 // 10.	Python Program for n-th Fibonacci number
+function fibonacci(n) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+      let temp = a;
+      a = b;
+      b = temp + b;
+    }
+    return b;
+  }
+  
+  const n = parseInt(prompt("Enter the value of n:"));
+  const result = fibonacci(n);
+  console.log(`The ${n}-th Fibonacci number is ${result}`);
+  
