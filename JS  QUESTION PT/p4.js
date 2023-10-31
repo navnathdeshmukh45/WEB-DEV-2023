154e1be679c37cb6a418861a5d0986063bb2580d
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-31 19:57:44 +0530

    PT JS

---
+++ JS  QUESTION PT/p4.js
@@ -0,0 +1,14 @@
+// non-primitive data type
+const user ={
+    id:101,
+    name:"ram",
+    age:45,
+};
+const user1 ={
+    id:102,
+    name:"Navnath",
+    age:23,
+};
+// console.log(user);
+// console.log(user1 );
+console.log("user value is ",  user  , " && ", "user1 value is ",user1 );
