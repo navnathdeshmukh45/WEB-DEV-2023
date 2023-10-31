29f068c26609573e4c9eae578d14ee8ae0649777
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-31 22:17:04 +0530

    JS PT 7

---
+++ JS  QUESTION PT/p7.js
@@ -0,0 +1,20 @@
+// object can also contain function
+let user= {
+    f : "priya",
+    l : "deshmukh",
+    age : 21,
+    user1: function(){
+    console.log(user);
+    }
+};
+user.user1()
+
+let user2 = {
+    f: "Navnath",
+    l: "Deshmukh",
+    age : 22,
+    us:function(){
+        console.log(user2);
+    }
+};
+user2.us()
