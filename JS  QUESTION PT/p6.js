c04cab882d693ed801db88a277091b38644c57a4
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-31 20:27:11 +0530

    PT JS 6 UPDATE
    user1 and user 2 update value id and age

--- JS  QUESTION PT/p6.js
+++ JS  QUESTION PT/p6.js
@@ -10,12 +10,12 @@ const user1={
      age:20,
 };
 // user1 update value id and age 
-// user1.id = 102;
-// user1.age = 22;
-// console.log(user1["id"]);
-// console.log(user1["age"]);
+user1.id = 102;
+user1.age = 22;
+console.log(user1["id"]);
+console.log(user1["age"]);
 // // user2 update id and age
-// user2.id = 103;
-// user2.age = 21;
-// console.log(user2['id']);
-// console.log(user2['age']);
+user2.id = 103;
+user2.age = 21;
+console.log(user2['id']);
+console.log(user2['age']);
