5dd0c1030d8cc99b222cc018312839db59902114
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-31 19:57:37 +0530

    PT JS

---
+++ JS  QUESTION PT/p3.js
@@ -0,0 +1,7 @@
+// primitive data type --> build in data type
+let nullvar = null;
+let numvar = 67;
+let strvar="navnath";
+let boolean = true;
+let symbolvar = symbol("nice boy")
+let undefine = undefined;
