01dfe611ae59b4603d82af99ba7ad8b83b291774
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-10-31 20:20:21 +0530

    Update p5.js

--- JS  QUESTION PT/p5.js
+++ JS  QUESTION PT/p5.js
@@ -1,4 +1,4 @@
-// object two show the output
+// object 
 const user1={
     id:101,
     name:"Navnath",
@@ -9,5 +9,13 @@ const user1={
      name:"priya",
      age:20,
 };
-console.log(user1);
-console.log(user2['name']);
+
+// object two show the output
+// console.log(user1);
+// console.log(user2);
+
+// find the user1 name , user2 id , user1 age and user2 name.
+console.log(user1["name"]);
+console.log(user2["id"]);
+console.log(user1["age"]);
+console.log(user2["name"]);
