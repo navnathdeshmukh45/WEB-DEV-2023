169010022f898ade13190aa4ffd53ac36db3cb24
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2023-11-01 19:12:20 +0530

    JS PT 10
    add  operator   in  js

--- JS  QUESTION PT/p10.js
+++ JS  QUESTION PT/p10.js
@@ -1,49 +1,79 @@
 // Arithmetic operators
-let a = 10;
-let b = 5;
+// let a = 10;
+// let b = 5;
 
-console.log("Arithmetic Operators:");
-console.log("Addition: a + b =", a + b);
-console.log("Subtraction: a - b =", a - b);
-console.log("Multiplication: a * b =", a * b);
-console.log("Division: a / b =", a / b);
-console.log("Modulus: a % b =", a % b);
-console.log("Exponentiation: a ** b =", a ** b);
+// console.log("Arithmetic Operators:");
+// console.log("Addition: a + b =", a + b);
+// console.log("Subtraction: a - b =", a - b);
+// console.log("Multiplication: a * b =", a * b);
+// console.log("Division: a / b =", a / b);
+// console.log("Modulus: a % b =", a % b);
+// console.log("Exponentiation: a ** b =", a ** b);
 
-// // Comparison operators
-console.log("\nComparison Operators:");
-console.log("Equal to: a == b =", a == b);
-console.log("Not equal to: a != b =", a != b);
-console.log("Greater than: a > b =", a > b);
-console.log("Less than: a < b =", a < b);
-console.log("Greater than or equal to: a >= b =", a >= b);
-console.log("Less than or equal to: a <= b =", a <= b);
+// // // Comparison operators
+// console.log("\nComparison Operators:");
+// console.log("Equal to: a == b =", a == b);
+// console.log("Not equal to: a != b =", a != b);
+// console.log("Greater than: a > b =", a > b);
+// console.log("Less than: a < b =", a < b);
+// console.log("Greater than or equal to: a >= b =", a >= b);
+// console.log("Less than or equal to: a <= b =", a <= b);
 
-// // Logical operators
-let x = true;
-let y = false;
+// // // Logical operators
+// let x = true;
+// let y = false;
 
-console.log("\nLogical Operators:");
-console.log("AND (&&): x && y =", x && y);
-console.log("OR (||): x || y =", x || y);
-console.log("NOT (!): !x =", !x);
+// console.log("\nLogical Operators:");
+// console.log("AND (&&): x && y =", x && y);
+// console.log("OR (||): x || y =", x || y);
+// console.log("NOT (!): !x =", !x);
 
-// Assignment operators
-let result = 0;
-console.log("\nAssignment Operators:");
-result += 5; // Equivalent to result = result + 5
-console.log("Add and assign: result += 5 =", result);
-result -= 3; // Equivalent to result = result - 3
-console.log("Subtract and assign: result -= 3 =", result);
-result *= 2; // Equivalent to result = result * 2
-console.log("Multiply and assign: result *= 2 =", result);
-result /= 4; // Equivalent to result = result / 4
-console.log("Divide and assign: result /= 4 =", result);
+// // Assignment operators
+// let result = 0;
+// console.log("\nAssignment Operators:");
+// result += 5; // Equivalent to result = result + 5
+// console.log("Add and assign: result += 5 =", result);
+// result -= 3; // Equivalent to result = result - 3
+// console.log("Subtract and assign: result -= 3 =", result);
+// result *= 2; // Equivalent to result = result * 2
+// console.log("Multiply and assign: result *= 2 =", result);
+// result /= 4; // Equivalent to result = result / 4
+// console.log("Divide and assign: result /= 4 =", result);
 
-// Increment and Decrement operators
-console.log("\nIncrement and Decrement Operators:");
-let count = 5;
-count++; // Increment count by 1
-console.log("Increment: count++ =", count);
-count--; // Decrement count by 1
-console.log("Decrement: count-- =", count);
+// // Increment and Decrement operators
+// console.log("\nIncrement and Decrement Operators:");
+// let count = 5;
+// count++; // Increment count by 1
+// console.log("Increment: count++ =", count);
+// count--; // Decrement count by 1
+// console.log("Decrement: count-- =", count);
+
+
+// ternary operator 
+// let age =20;
+// let adult =(age >=18) ? "adult" :"minor";
+// console.log(adult);
+
+// string concatrnation oprator 
+// let firstName = "Navnath";
+// let lastName = "Deshmukh";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName); 
+
+// instance of operator 
+// function Person() {}
+// let person = new Person();
+// console.log(person instanceof Person); // Output: true
+
+// // type of operator
+// let num = 42;
+// console.log(typeof num); // Output: "number"
+
+// Conditional (Comma) Operator (,):
+// let x = (1 + 2, 3 + 4);
+// console.log(x); // Output: 7
+
+// void operator 
+let result = void 0;
+console.log(result); // Output: undefined
+
